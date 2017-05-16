12cd4ad137548a2aeda42419aea8875d04f24522
Bryce Covert <git@brycecovertoperations.com>
2017-05-16 13:41:42 -0700

    mouse look, layers.

--- src/textured_quad/core.clj
+++ src/textured_quad/core.clj
@@ -3,9 +3,11 @@
   (:import [com.badlogic.gdx ApplicationAdapter Gdx ApplicationListener]
            [com.badlogic.gdx.graphics GL20 GL30 Texture Mesh VertexAttribute VertexAttributes$Usage Color PerspectiveCamera]
            [com.badlogic.gdx.graphics.g3d Model ModelBatch ModelInstance Material Environment]
+           [com.badlogic.gdx.graphics.g3d.environment PointLight]
            [com.badlogic.gdx.graphics.g3d.attributes ColorAttribute TextureAttribute]
-           [com.badlogic.gdx.graphics.g3d.utils ModelBuilder ]
+           [com.badlogic.gdx.graphics.g3d.utils ModelBuilder CameraInputController FirstPersonCameraController]
            [com.badlogic.gdx.graphics.glutils ShaderProgram]
+           [com.badlogic.gdx Input$Keys]
            [com.badlogic.gdx.backends.lwjgl LwjglApplication]
            [com.badlogic.gdx.math Vector2 Intersector Vector3 Matrix4]
            
@@ -18,7 +20,9 @@
         model-batch (atom nil)
         tex (atom nil)
         cam (atom nil)
-        environment (atom nil)]
+        cont (atom nil)
+        environment (atom nil)
+        light (atom nil)]
     (reify 
       ApplicationListener
       (create [this]
@@ -33,28 +37,69 @@
                               (Material. (into-array [(TextureAttribute/createDiffuse @tex)]))
                               (bit-or VertexAttributes$Usage/Position VertexAttributes$Usage/Normal VertexAttributes$Usage/TextureCoordinates))
 
-              m [[1 1 1 1 1 1 1 1 1 1]
-                   [1 1 1 0 1 0 0 1 1 1]
-                   [1 1 1 0 1 0 0 1 1 1]
-                   [1 0 0 0 0 0 1 1 1 1]
-                   [1 1 1 0 0 0 0 0 0 1]
-                   [1 1 1 0 0 0 0 0 1 1]
-                   [1 1 1 0 0 0 0 1 1 1]
-                   [1 1 1 1 0 1 1 1 1 1]
+              m [[[1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 1 1 1 1 1 1]
+                  [1 1 1 1 1 1 1 1 1 1]]
+                 [[1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 1 1 1 1 1 1]
                    [1 1 1 0 0 0 1 1 1 1]
-                   [1 1 1 1 1 1 1 1 1 1]]]
+                   [1 1 1 0 0 0 0 1 1 1]
+                   [1 1 1 0 0 0 0 1 1 1]
+                   [1 1 1 0 0 0 0 1 1 1]
+                   [1 1 1 1 0 0 0 1 1 1]
+                   [1 1 1 0 0 0 0 0 1 1]
+                  [1 1 1 1 1 1 1 1 1 1]]
+                 [[1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 1 1 1 1 1 1]
+                   [1 1 1 1 0 0 1 1 1 1]
+                   [1 1 1 1 0 0 0 1 1 1]
+                   [1 1 1 1 0 0 0 1 1 1]
+                   [1 1 1 1 0 0 0 1 1 1]
+                   [1 1 1 1 0 0 0 1 1 1]
+                   [1 1 1 0 0 0 0 0 1 1]
+                  [1 1 1 1 1 1 1 1 1 1]]
+                 [[1 1 1 1 1 1 1 1 1 1]
+                  [1 1 1 1 1 1 1 1 1 1]
+                  [1 1 1 1 1 1 1 1 1 1]
+                  [1 1 1 1 1 1 1 1 1 1]
+                  [1 1 1 1 1 1 1 1 1 1]
+                  [1 1 1 1 1 1 1 1 1 1]
+                  [1 1 1 1 1 1 1 1 1 1]
+                  [1 1 1 1 1 1 1 1 1 1]
+                  [1 1 1 1 1 1 1 1 1 1]
+                  [1 1 1 1 1 1 1 1 1 1]]]]
+          (reset! light (doto (PointLight.)
+                                        (.set 1.0 1.0 1.0 0.0 0.0 0.35 2.5)))
           (reset! environment (doto (Environment.)
-                                (.set (ColorAttribute. ColorAttribute/AmbientLight 0.8 0.8 0.8 1.0))))
+                                (.add @light)
+                                #_(.add (doto (PointLight.)
+                                        (.set 0.6 0.6 1.0 -1.0 2.0 0.35 4.0)))
+                                (.set (ColorAttribute. ColorAttribute/AmbientLight 0.01 0.01 0.01 0.1))))
 
           ;; todo change this to map
           (reset! model-instances
-                  (for [[row y] (map vector m (range))
+                  (for [[level l] (map vector m (range))
+                        [row y] (map vector level (range))
                         [wall? x] (map vector row (range))
                         :when (= 1 wall?)]
                     (do
                       
-                      (ModelInstance. box (float (- x 5)) (float (- y 5)) 0.35))))
+                      (ModelInstance. box (float (- x 5)) (float (- y 5)) (- (* 0.6519 (- l 0)) (/ 0.6519 2))))))
           (reset! model-batch (ModelBatch.))
+
+          
+          (.setInputProcessor Gdx/input (reset! cont (doto (FirstPersonCameraController. cam)
+                                                       #_(#(set! (.forwardKey %) Input$Keys/W ))
+                                                       )))
           ) 
         nil)
       (dispose [this] nil)
@@ -63,7 +108,10 @@
       (render [this]
         (.glClearColor Gdx/gl 0 0 0 1)
         (.glClear Gdx/gl (bit-or GL30/GL_COLOR_BUFFER_BIT GL30/GL_DEPTH_BUFFER_BIT))
-        (.rotateAround @cam Vector3/Zero (Vector3. 0 0 1) 0.5)
+        #_(.rotateAround @cam Vector3/Zero (Vector3. 0 0 1) 0.5)
+        #_(.add (.position @cam) 0.0 0.01 0)
+        (.update @cont)
+        (.set (.position @light) (.position @cam))
         (.update @cam)
         (.begin @model-batch @cam)
         (doseq [m @model-instances]
