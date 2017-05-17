889a1a57de79e97a2d5a2fc3163d2fd118cbfb28
Bryce Covert <git@brycecovertoperations.com>
2017-05-16 17:07:53 -0700

    3d and lights.

--- src/textured_quad/core.clj
+++ src/textured_quad/core.clj
@@ -1,12 +1,13 @@
 (ns textured-quad.core
   
   (:import [com.badlogic.gdx ApplicationAdapter Gdx ApplicationListener]
-           [com.badlogic.gdx.graphics GL20 GL30 Texture Mesh VertexAttribute VertexAttributes$Usage Color PerspectiveCamera]
+           [com.badlogic.gdx.graphics GL20 GL30 Texture Mesh VertexAttribute VertexAttributes$Usage Color PerspectiveCamera Pixmap$Format]
            [com.badlogic.gdx.graphics.g3d Model ModelBatch ModelInstance Material Environment]
            [com.badlogic.gdx.graphics.g3d.environment PointLight]
            [com.badlogic.gdx.graphics.g3d.attributes ColorAttribute TextureAttribute]
+           [com.badlogic.gdx.graphics.g2d SpriteBatch]
            [com.badlogic.gdx.graphics.g3d.utils ModelBuilder CameraInputController FirstPersonCameraController]
-           [com.badlogic.gdx.graphics.glutils ShaderProgram]
+           [com.badlogic.gdx.graphics.glutils ShaderProgram FrameBuffer]
            [com.badlogic.gdx Input$Keys]
            [com.badlogic.gdx.backends.lwjgl LwjglApplication]
            [com.badlogic.gdx.math Vector2 Intersector Vector3 Matrix4]
@@ -22,7 +23,9 @@
         cam (atom nil)
         cont (atom nil)
         environment (atom nil)
-        light (atom nil)]
+        light (atom nil)
+        fbo (atom nil)
+        sb (atom nil)]
     (reify 
       ApplicationListener
       (create [this]
@@ -30,7 +33,7 @@
         (let [cam (reset! cam (doto (PerspectiveCamera. 48.262 320 240)
                                 (.rotate (Vector3. 1 0 0) 85.8)
                                 (#(.set (.position %) 0 0 0.41))
-                                (#(set! (.near %) 0.01))))
+                                (#(set! (.near %) 0.5))))
               _ (.update cam)
               box (.createBox (ModelBuilder.)
                               1 1 0.6519
@@ -78,7 +81,10 @@
                   [1 1 1 1 1 1 1 1 1 1]
                   [1 1 1 1 1 1 1 1 1 1]]]]
           (reset! light (doto (PointLight.)
-                                        (.set 1.0 1.0 1.0 0.0 0.0 0.35 2.5)))
+                                        (.set 1.0 1.0 1.0 0.0 0.0 0.35 4.5)))
+                          
+          (reset! sb (SpriteBatch.))
+          (reset! fbo (FrameBuffer. Pixmap$Format/RGBA8888 160 120 true))
           (reset! environment (doto (Environment.)
                                 (.add @light)
                                 #_(.add (doto (PointLight.)
@@ -106,8 +112,10 @@
       (pause [this] nil)
       (resume [this] nil)
       (render [this]
+        (.begin @fbo)
         (.glClearColor Gdx/gl 0 0 0 1)
         (.glClear Gdx/gl (bit-or GL30/GL_COLOR_BUFFER_BIT GL30/GL_DEPTH_BUFFER_BIT))
+        (.glEnable Gdx/gl GL30/GL_DEPTH_TEST)
         #_(.rotateAround @cam Vector3/Zero (Vector3. 0 0 1) 0.5)
         #_(.add (.position @cam) 0.0 0.01 0)
         (.update @cont)
@@ -116,7 +124,14 @@
         (.begin @model-batch @cam)
         (doseq [m @model-instances]
           (.render @model-batch m @environment))
-        (.end @model-batch))
+        (.end @model-batch)
+        (.end @fbo)
+        #_(.resize @sb 640 480)
+        (.glClearColor Gdx/gl 0 0 0 1)
+        (.glClear Gdx/gl (bit-or GL30/GL_COLOR_BUFFER_BIT GL30/GL_DEPTH_BUFFER_BIT))
+        (.begin @sb)
+        (.draw @sb (.getColorBufferTexture @fbo) (float 0.0) (float 0.0) (float 640) (float 480))
+        (.end @sb))
       (resize [this width height]
         nil))))
 
